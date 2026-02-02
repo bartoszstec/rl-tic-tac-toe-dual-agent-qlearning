2bc8400570c9653df33ddfa0de1918d39f9ea061
Bartosz Stec <bartoszstec7@gmail.com>
2026-02-02 20:02:23 +0100

    workflow changes, add port in app.py

--- app.py
+++ app.py
@@ -158,4 +158,5 @@ def player_move():
                     "draw": draw})
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
